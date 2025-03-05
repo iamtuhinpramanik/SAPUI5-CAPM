f2ac553654a4cb81336eb95826b205a3cf9ee489
Tuhin Pramanik <tuhin.pramanik@sumodigitech.com>
2025-03-05 15:11:45 +0000

    New Changes

--- webapp/controller/EmployeeList.controller.js
+++ webapp/controller/EmployeeList.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller,JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
+], (Controller,JSONModel,MessageBox,MessageToast) => {
     "use strict";
 
     return Controller.extend("com.mycompany.employeemanagement.controller.EmployeeList", {
@@ -10,9 +12,26 @@ sap.ui.define([
             this.getView().setModel(oModel,"Employee")
             
         },
-        onViewDetails:function(){
-            alert("Details should be load here");
-            
+        onViewDetails: function (oEvent) {
+            var oItem = oEvent.getSource().getParent();
+            var oContext = oItem.getBindingContext("Employee");
+            var oEmployee = oContext.getObject();
+
+            // Display Employee Details in a MessageBox
+            MessageBox.success(
+                "Employee Details:\n\n" +
+                "Name : " + oEmployee.EmployeeName + "\n" +
+                "Position : " + oEmployee.Position + "\n" +
+                "Email : " + oEmployee.EmployeeEmail + "\n" +
+                "Phone : " + oEmployee.EmployeePhoneNo +"\n"+
+                "Position : " + oEmployee.Position + "\n" +
+                "Salary : " + oEmployee.salary + " USD",
+                {
+                    title: "Employee Info"
+                }
+            );
         }
     });
+
+    
 });
